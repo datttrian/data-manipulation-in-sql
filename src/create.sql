49f8d91a794b3a382d5c2fe70938342c85f32ee8
datttrian <datttrian@gmail.com>
2024-05-03 14:57:00 +0000

    Fri, May 3, 2024, 2:57 PM +00:00

--- src/create.sql
+++ src/create.sql
@@ -13,5 +13,13 @@ CREATE TABLE country (
     name VARCHAR(255)
 );
 
+CREATE TABLE team (
+    id INT PRIMARY KEY,
+    team_api_id INT,
+    team_long_name VARCHAR(255),
+    team_short_name VARCHAR(10)
+);
+
 \copy match FROM '/tmp/match.csv' DELIMITER ',' CSV HEADER;
 \copy country FROM '/tmp/country.csv' DELIMITER ',' CSV HEADER;
+\copy team FROM '/tmp/team.csv' DELIMITER ',' CSV HEADER;
